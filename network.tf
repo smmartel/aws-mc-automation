4ac0f7a740bc48806a64094b0370bf4b5e8c43e5
Shane Martel <shnemrtel@gmail.com>
2026-03-09 12:04:43 -0400

    added missing region to network file

--- network.tf
+++ network.tf
@@ -13,6 +13,8 @@ resource "aws_subnet" "minecraft_subnet" {
   vpc_id = aws_vpc.minecraft_vpc.id
   cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true # Connect to the server from the internet
+
+  availability_zone = "us-east-1a" # Specify the availability zone for the subnet. Adjust as needed.
     tags = {
         Name = "minecraft-subnet"
     }
